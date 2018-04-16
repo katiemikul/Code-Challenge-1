128d3c74a484bbe9b0dc332894c1e13a8e79c8e3
Katie Mikul <katie.heding@gmail.com>
2018-04-16 14:14:03 -0500

    added swap and delete buttons

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ console.log('JQuery is ready!');
 function onReady(){
 console.log('JQuery is loaded');
 $('.generateButton').on('click', changeTheDiv);
+$('.appendTheDiv').css('background-color', 'red');
+addingButtons();
 }
 
 function changeTheDiv(){
     console.log('Changing the div function');
     $('.appendTheDiv').empty();
     $('.appendTheDiv').append("Changing the div!").val('');
-    $('.appendTheDiv').css('background-color', 'red');
     countingTheClicks();
 }
 
@@ -23,4 +24,11 @@ function countingTheClicks(){
         clickCounter++;
         $("#countingThings").append('<p id="countingThings">Number of clicks:' +clickCounter+ '</p>');
         });
+}
+
+function addingButtons(){
+    console.log('Adding buttons?');
+$('.appendTheDiv').append("<button>Swap</button>");
+$('.appendTheDiv').append("<button>Delete</button>");
+
 }
