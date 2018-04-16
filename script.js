6730ffd4b2557efe544018fe5ee8ace37ef9cb4d
Katie Mikul <katie.heding@gmail.com>
2018-04-16 13:58:44 -0500

    add the click counter

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(onReady);
 console.log('JQuery is ready!');
 
+
 function onReady(){
 console.log('JQuery is loaded');
 $('.generateButton').on('click', changeTheDiv);
@@ -10,4 +11,15 @@ function changeTheDiv(){
     console.log('Changing the div function');
     $('.appendTheDiv').empty();
     $('.appendTheDiv').append("Changing the div!").val('');
+    countingTheClicks();
+}
+
+function countingTheClicks(){
+    let clickCounter = 0;
+    console.log("Hopefully counting the clicks");
+    $('.appendTheDiv').append('<p id="countingThings">Number of clicks:</p>');
+    $(".generateButton").click(function() {
+        clickCounter++;
+        $("#countingThings").append('<p id="countingThings">Number of clicks:' +clickCounter+ '</p>');
+        });
 }
