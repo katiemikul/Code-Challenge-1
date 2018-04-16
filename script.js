ae78b91f2038f855cef5295d7a648f46634cf539
Katie Mikul <katie.heding@gmail.com>
2018-04-16 13:33:07 -0500

    change the div

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('jQuery is working!');
+$(document).ready(onReady);
+console.log('JQuery is ready!');
+
+function onReady(){
+console.log('JQuery is loaded');
+$('.generateButton').on('click', changeTheDiv);
+}
+
+function changeTheDiv(){
+    console.log('Changing the div function');
+    $('.appendTheDiv').empty();
+    $('.appendTheDiv').append("Changing the div!").val('');
+}
